e2a056a2bdd5ee6333709430a305c9db31bb3bf9
Lukasz Magiera <LukaszMagiera26@gmail.com>
2019-04-10 21:34:48 +0200

    Up

---
+++ DependencyInjectionObjectVehicle/src/App.java
@@ -0,0 +1,11 @@
+public class App {
+
+    public static void main(String[] args) {
+
+
+    Vehicle raceCar = new Vehicle(new LargeEngine(500));
+    raceCar.crankIgnition();
+
+}
+
+}
---
+++ DependencyInjectionObjectVehicle/src/SmallEngine.java
@@ -0,0 +1,13 @@
+public class SmallEngine implements Engine {
+
+    private int horsePower;
+
+    public SmallEngine(int horsePower) {
+        this.horsePower = horsePower;
+    }
+
+    @Override
+    public void startEngine() {
+
+    }
+}
